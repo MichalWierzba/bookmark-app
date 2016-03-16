b66414e749b0bf893d3a3bd9f0bf837b29be422b
Michał Wierzba <unknown>
2016-03-16 11:13:57 +0100

    Extract tags list to directive

--- app/components/bookmarks/list/bookmarks-list.directive.js
+++ app/components/bookmarks/list/bookmarks-list.directive.js
@@ -4,12 +4,10 @@ angular.module('bookmarks-module')
             templateUrl: 'app/components/bookmarks/list/bookmarks-list.html',
             link: function ($scope) {
                 $scope.activeFilter = $routeParams.tag || '';
-                $scope.tags = bookmarksService.tags;
                 $scope.bookmarks = bookmarksService.bookmarks;
 
                 $scope.deleteBookmark = function (bookmark) {
                     bookmarksService.delete(bookmark);
-                    $scope.tags = bookmarksService.retrieveTagsFromBookmarks();
                 };
             }
         }
---
+++ app/components/bookmarks/tags/bookmarks-tags.directive.js
@@ -0,0 +1,9 @@
+angular.module('bookmarks-module')
+    .directive('bookmarksTags', function (bookmarksService) {
+        return {
+            templateUrl: 'app/components/bookmarks/tags/bookmarks-tags.html',
+            link: function ($scope) {
+                $scope.tags = bookmarksService.tags;
+            }
+        }
+    })
